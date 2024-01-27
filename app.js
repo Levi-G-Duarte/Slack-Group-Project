c7145d84aa6dd5b2cac4e37f90410eb41c970470
Levi Duarte <lduart441@west-mec.org>
2024-01-26 17:23:39 -0700

    12624

---
+++ app.js
@@ -0,0 +1,26 @@
+////////////////////////////////////////////////////////////////////// BODY JS
+
+let clicked = false;
+
+// aboutNav.addEventListener('click', function(evt) {
+//     let target = evt.target;
+//     if (target.id === 'nav-about') {
+// mainBody.style.border = "thick solid black";
+// function myFunction() {
+//     mainBody.style.border = "none";
+// }
+//     }
+// })
+
+function changeColor(color) {
+    document.mainBody.style.background = color;
+}
+function myFunc() {
+    changeColor('black');
+}
+
+
+
+
+
+
